58684ef644aafca91d92617afe4e4d1a829b00bc
Elcins <elcins.gasanovs@gmail.com>
2021-12-01 21:18:43 +0200

    mod2-task1

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import {createFilmContainerTemplate} from './view/film-container-view.js';
 import {createShowMoreBtnTemplate} from './view/show-more-btn-view.js';
 import {createFilmsCardsTemplate} from './view/films-cards-view.js';
 import {createAdditionalFilmInfoPopupTemplate} from './view/additional-info-popup-view.js';
+import {getFilmsData} from './mock/mock-data.js';
 
-const TASK_COUNT = 5;
+const FILM_COUNT = 5;
+
+const films = getFilmsData();
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
@@ -17,9 +20,29 @@ renderTemplate(siteMainElement, createFilmContainerTemplate(), RenderPosition.BE
 
 const siteFilmListContainer = siteMainElement.querySelector('.films-list__container');
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  renderTemplate(siteFilmListContainer, createFilmsCardsTemplate(), RenderPosition.BEFOREEND);
-}
+films.slice(0, 5).forEach((film) => {
+  renderTemplate(siteFilmListContainer, createFilmsCardsTemplate(film), RenderPosition.BEFOREEND);
+});
 
-renderTemplate(siteFilmListContainer, createShowMoreBtnTemplate(), RenderPosition.AFTEREND);
-renderTemplate(document.body, createAdditionalFilmInfoPopupTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(document.body, createAdditionalFilmInfoPopupTemplate(films[0]), RenderPosition.BEFOREEND);
+
+if (films.length > FILM_COUNT) {
+  let renderFilmCount = FILM_COUNT;
+
+  renderTemplate(siteFilmListContainer, createShowMoreBtnTemplate(), RenderPosition.AFTEREND);
+
+  const loadMoreBtn = document.body.querySelector('.films-list__show-more');
+
+  loadMoreBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    films
+      .slice(renderFilmCount, renderFilmCount + FILM_COUNT)
+      .forEach((film) => renderTemplate(siteFilmListContainer, createFilmsCardsTemplate(film), RenderPosition.BEFOREEND));
+
+    renderFilmCount += FILM_COUNT;
+
+    if (renderFilmCount >= films.length) {
+      loadMoreBtn.remove();
+    }
+  });
+}
---
+++ src/mock/mock-data.js
@@ -0,0 +1,139 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const getRandomArbitrary = function (min, max, float) {
+  if (min >= 0 && max >= min) {
+    const randomNumber = Math.random() * (max - min) + min;
+    return randomNumber.toFixed(float);
+  }
+};
+
+const shuffleArray = function (data) {
+  const array = data.slice();
+  for (let itt = array.length - 1; itt > 0; itt--) {
+    const jtt = Math.floor(Math.random() * (itt + 1));
+    const temp = array[itt];
+    array[itt] = array[jtt];
+    array[jtt] = temp;
+  }
+  return array;
+};
+
+const generateFilmTitle = () => {
+  const titles = [
+    'The Dance of Life',
+    'Sagebrush Trail',
+    'The Man with the Golden Arm',
+    'Santa Claus Conquers the Martians',
+    'Popeye the Sailor Meets Sindbad the Sailor',
+    'The Great Flamarion',
+    'Made for Each Other'
+  ];
+
+  return titles[getRandomInt(0, titles.length - 1)];
+};
+
+const generateFilmPoster = () => {
+  const images = [
+    'images/posters/made-for-each-other.png',
+    'images/posters/popeye-meets-sinbad.png',
+    'images/posters/sagebrush-trail.jpg',
+    'images/posters/santa-claus-conquers-the-martians.jpg',
+    'images/posters/the-dance-of-life.jpg',
+    'images/posters/the-great-flamarion.jpg',
+    'images/posters/the-man-with-the-golden-arm.jpg'
+  ];
+  return images[getRandomInt(0, images.length - 1)];
+};
+
+const generateFilmDescription = () => {
+  const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
+  const descriptions = lorem.split('. ');
+  return descriptions.slice(0, getRandomInt(0, descriptions.length)).join('. ');
+};
+
+const generateRandomEmoji = () => {
+  const emojis = [
+    'smile',
+    'puke',
+    'sleeping',
+    'angry'
+  ];
+  return emojis[getRandomInt(0, emojis.length - 1)];
+};
+
+const people = ['Lee Aaker',
+  'Willie Aames',
+  'Quinton Aaron',
+  'Victor Aaron',
+  'Abbott and Costello',
+  'Bruce Abbott',
+  'Bud Abbott',
+  'Christopher Abbott',
+  'Philip Abbott',
+  'Jake Abel',
+  'Walter Abel',
+  'Zachary Abel',
+  'F.Murray Abraham',
+  'Jon Abrahams',
+  'Omid Abtahi',
+  'Yousef Abu - Taleb',
+  'Kirk Acevedo'
+];
+
+const generatePeople = (n) => {
+  const randomPeople = shuffleArray(people);
+  return randomPeople.slice(0, n);
+};
+
+const generateComment = () => ({
+  emoji: generateRandomEmoji(),
+  data: '2019/12/31 23:59',
+  author: 'user',
+  text: generateFilmDescription(),
+});
+
+const generateCountry = () => {
+  const countries = [
+    'Portugal',
+    'Great Britain',
+    'Estonia',
+    'Ukraine',
+    'France',
+    'Netherlands',
+    'Norway'
+  ];
+  return countries[getRandomInt(0, countries.length - 1)];
+};
+
+const generateGenre = () => {
+  const genres = [
+    'Comedy',
+    'Thriller',
+    'Drama',
+    'Western',
+    'Documentary'
+  ];
+  return genres[getRandomInt(0, genres.length - 1)];
+};
+
+const generateFilmData = () => ({
+  title: generateFilmTitle(),
+  poster: generateFilmPoster(),
+  description: generateFilmDescription(),
+  comment: new Array(getRandomInt(0, 10)).fill({}).map(generateComment),
+  rating: getRandomArbitrary(0, 10, 1),
+  director: generatePeople(1),
+  screenwriters: generatePeople(2),
+  actors: generatePeople(3),
+  release: '01 April 1995',
+  length: '1h 36m',
+  country: generateCountry(),
+  genre: generateGenre(),
+  ageRating: getRandomInt(0, 18),
+});
+
+export const getFilmsData = () => new Array(getRandomInt(3, 5) * 5).fill({}).map(generateFilmData);
